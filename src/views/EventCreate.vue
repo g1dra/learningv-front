c5e1a752d7bf18a7338ca982acb4585fa3b733aa
Darko Vucetic <darko@kod.io>
2019-02-26 16:01:09 +0100

    day two

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEvent(1) }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">
+        {{ cat }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+export default {
+  computed: {
+    catLength: state => {
+      return state.categories.length;
+    },
+    ...mapState(["categories", "user"]),
+    getEvent() {
+      return this.$store.getters.getEventById;
+    }
+  }
+};
 </script>
 
 <style></style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
-    <h1>Events List</h1>
-    <EventCard />
-    <BaseIcon />
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
-
 <script>
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: [],
+      event: String
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data; // <--- set the events data
+      })
+      .catch(error => {
+        this.error = error.response;
+      });
   }
 };
 </script>
-
-<style></style>
